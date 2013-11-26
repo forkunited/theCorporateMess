29dd4dabe06147653aa22350bd64de29fb68b15c
Bill McDowell <forkunited@gmail.com>
2013-11-26 13:39:08 -0500

    Tried to fix jitteriness in visualization

--- scripts/VisualAnimationSprings.js
+++ scripts/VisualAnimationSprings.js
@@ -108,7 +108,13 @@ function VisualAnimationSprings() {
 			clusterDensity = 2.0*clusterEdgeCount/(clusterSize*(clusterSize - 1.0));
 		}
 		
-		for (v2 in activeVerts) {
+		var shuffledVerts = [];
+		for (var i = 0; i < activeVerts.length; i++)
+			shuffledVerts.push(activeVerts[i]);
+		shuffledVerts = shuffle(shuffledVerts);
+		
+		for (var i = 0; i < Math.min(20, shuffledVerts.length); i++) {
+			var v2 = shuffledVerts[i];
 			if (vert.getID() != v2) {
 				var vert2 = idsToVerts[v2];
 				var dVX = vert.getX() - vert2.getX();
@@ -194,5 +200,28 @@ function VisualAnimationSprings() {
 			clusterForces[clusterId].x += clusterFX;
 			clusterForces[clusterId].y += clusterFY;
 		}
+	}
+	
+	/* From: http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
+	function shuffle(array) {
+	  var currentIndex = array.length
+		, temporaryValue
+		, randomIndex
+		;
+
+	  // While there remain elements to shuffle...
+	  while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	  }
+
+	  return array;
 	}
 }
